7d224f58a639a8a0f27349a52ba49d29a4dc7f73
Pierricles <pierromanuel5@gmail.com>
2018-02-25 16:44:24 +0100

    update add oven

--- Machine.js
+++ Machine.js
@@ -2,13 +2,31 @@ Oven=function(sprite,posx,posy,groupe){
   this.oven = groupe.create(posx,posy,sprite);
   this.oven.animations.add('actif', [1, 2, 3,4], 10, true);
   this.oven.frame=0;
-  this.stock=null;
-  this.oven.animations.play('actif');
+  this.stock=0;
+  this.use=false
+
 }
 
-Oven.prototype.dropin=function(){
-console.log('interaction');
+Oven.prototype.drop=function(itemId){
+  if((this.stock != 0 && itemId != 0)||this.use==true){
+    return itemId;
+  }else if(this.stock!=0 && itemId==0){
+    let tmp = this.stock;
+    this.stock = 0;
+
+    this.use=false
+    this.oven.frame=0;
+    return tmp;
+  }else if(this.stock == 0 && itemId != 0){
+		this.stock = itemId;
+
+    this.oven.animations.play('actif');
+    this.use=true
+		return 0;
 }
+return itemId;
+}
+
 
 Table = function(sprite,posx,posy,groupe){
 	this.table = groupe.create(posx,posy,sprite);
