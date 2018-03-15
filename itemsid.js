519eb25b3b08cdc7ab955fd43a78b2eb853ed810
Pierricles <pierromanuel5@gmail.com>
2018-03-15 13:38:28 +0100

    add itemid

--- itemsid.js
+++ itemsid.js
@@ -1,5 +1,38 @@
 var itemsId = Object.freeze({
   pneu: 1,
-});
+  Sceau: NaN,
+  Metal:NaN,
+  Verre:NaN,
+  Papier:NaN
+  Vitre:NaN,
+  PaquetPapier:NaN,
+  PlaqueMetal:NaN,
+  SceauPneu1:NaN,
+  SceauPneu2:NaN,
+  SceauPneu3:NaN,
+  SceauPneu0:NaN,
+  SceauVerre0:NaN,
+  SceauVerre1:NaN,
+  SceauVerre2:NaN,
+  SceauVerre3:NaN,
+  SceauMetal0:NaN,
+  SceauMetal1:NaN,
+  SceauMetal2:NaN,
+  SceauMetal3:NaN,
+  SceauPlastique0:NaN,
+  SceauPlastique1:NaN,
+  SceauPlastique2:NaN,
+  SceauPlastique3:NaN,
+  BlockPlastique1:NaN,
+  BlockPlastique2:NaN,
+  BlockPlastique3:NaN,
+  BlockMetal1:NaN,
+  BlockMetal2:NaN,
+  BlockMetal3:NaN,
+  BlockPapier1:NaN,
+  BlockPapier2:NaN,
+  BlockPapier3:NaN,
 
-alert(itemsId.pneu);
+  CartonPate:NaN,
+
+});
