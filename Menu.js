c0ead83b0c45c0c2e7396f3bdeff78d5b20753bd
Unknown <pierre.marez@isen.yncrea.fr>
2018-03-18 14:52:41 +0100

    Correction bug musique

--- Menu.js
+++ Menu.js
@@ -1,22 +1,48 @@
+//Menu générale
 var Menu = {
   preload : function(){
-    Menu.load.audio('testmus','musics/testmus.mp3');
+    Menu.load.audio('testmus','musics/testmus.mp3');//Musique du menu
     Menu.load.image('title','assets/banner.png');
     Menu.load.image('button1','assets/table.png');
   },
   create : function(){
-    let music = Menu.add.audio('testmus');
-    music.play("",0,0.5,true);
+    musicMenu = Menu.add.audio('testmus');
+    musicMenu.play("",0,0.5,true);
     let button = Menu.add.button(Menu.world.centerX, Menu.world.centerY, 'button1', goMenuGame,this,0,0,0);
     button.anchor.setTo(0.5,0.5);
+    let btt = Menu.add.button(Menu.world.centerX, Menu.world.centerY+32,'button1',goMenuOpt, this,0,0,0);
+    btt.anchor.setTo(0.5,0.5);
   }
 }
   let goMenuGame =  function(){
+    musicMenu.pause();//met la musique en pause pour le changement de page
     this.state.start('MenuGame');
+  }
+  let goMenuOpt = function(){
+    musicMenu.pause();//met la musique en pause pour le changement de page
+    this.state.start('MenuOpt');
   }
 
 var MenuGame ={
   preload : function(){
-    
+    MenuGame.load.image('title','assets/banner.png');
+    MenuGame.load.image('button2','assets/table.png');
+  },
+  create : function(){
+    musicMenu.resume();//relance la musique là ou elle s'était arrêtée
+    let button = MenuGame.add.button(MenuGame.world.centerX, MenuGame.world.centerY, 'title');
+    button.anchor.setTo(0.5,0.5);
+  }
+}
+
+var MenuOpt ={
+  preload: function(){
+    MenuOpt.load.image('title','assets/banner.png');
+    MenuOpt.load.image('button3','assets/table.png');
+  },
+  create : function(){
+    musicMenu.resume();//relance la musique là ou elle s'était arrêtée 
+    let button = MenuOpt.add.button(MenuOpt.world.centerX,MenuOpt.world.centerY, 'button3');
+    button.anchor.setTo(0.5,0.5);
   }
 }
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var jeu = new Phaser.Game(1000, 300, Phaser.AUTO, '');
 
+//Donne les différents menus
 jeu.state.add('Menu', Menu);
 jeu.state.add('MenuGame', MenuGame);
-//jeu.state.add('MenuOpt', MenuOpt);
+jeu.state.add('MenuOpt', MenuOpt);
+//Donne la fenetre de jeu
 //jeu.state.add('Game', Game);
 
+//On commence au Menu générale
 jeu.state.start('Menu');
