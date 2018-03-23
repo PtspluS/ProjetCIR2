79564c70ff8b0dda0b3eae38f346e7980b156524
Unknown <antoine.carlier@isen.yncrea.fr>
2018-03-23 20:14:01 +0100

    Back in options

--- Menu.js
+++ Menu.js
@@ -105,12 +105,12 @@ var MenuOpt ={
   preload: function(){
     MenuOpt.load.image('title','assets/banner.png');
     MenuOpt.load.image('button3','assets/table.png');
-    MenuOpt.load.image('arrow','assets/arrow.png')
+    MenuGame.load.spritesheet('back','assets/backbutton.png',68,84);
   },
   create : function(){
     musicMenu.resume();//relance la musique là ou elle s'était arrêtée
     let button1 = MenuOpt.add.button(MenuOpt.world.centerX,MenuOpt.world.centerY, 'button3');
     button1.anchor.setTo(0.5,0.5);
-    let back = MenuGame.add.button(0,0,'arrow',returnMenu,this,0,0,0);
+    let back = MenuGame.add.button(0,0,'back',returnMenu,this,1,0,2);
   }
 }
