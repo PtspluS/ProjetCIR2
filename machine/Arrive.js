924fbd8089fd99b75e8b20c65f7ce8c01e4d4301
Pierricles <pierromanuel5@gmail.com>
2018-04-24 17:34:04 +0200

    bug arrive supprimé

--- machine/Arrive.js
+++ machine/Arrive.js
@@ -6,18 +6,38 @@ Arrive = function(sprite,posx,posy,groupe,tabEl,time,pattern){
 	this.time = time;
 	this.pattern = pattern;
 	this.patnum = 0;
+	this.posx=posx+20;
+	this.posy=posy+20;
+	this.scoretext = game.add.bitmapText(this.posx,this.posy, 'fontred', '', 30);
+		game.physics.arcade.enable([ this.scoretext ]);
 
 	game.time.events.add(this.time, () => {this.envoie();} , this);
 }
 
 Arrive.prototype.envoie = function(){
 	var infront = map[Math.round(parseInt((this.arrive.y + 26)/64))+1][Math.round(parseInt((this.arrive.x)/64))];
-	if(infront.recup != undefined){
-		infront.recup(this.pattern ? this.tab[this.patnum++ % this.tab.length] : this.tab[Math.floor(Math.random() * this.tab.length)], 'down');
-	}else{
-		infront.drop(this.pattern ? this.tab[this.patnum++ % this.tab.length] : this.tab[Math.floor(Math.random() * this.tab.length)]);
-	}
+	console.log('a');
+if(infront.drop(this.pattern ? this.tab[this.patnum++ % this.tab.length] : this.tab[Math.floor(Math.random() * this.tab.length)])!=0){
+	console.log('ai');
+	this.addscore(-100);
+};
+
+
 	game.time.events.add(this.time, () => {this.envoie();} , this);
+}
+
+Arrive.prototype.addscore=function(value){
+	game.world.bringToTop(this.scoretext);
+this.scoretext.text=value;
+this.scoretext.body.velocity.y=-50;
+game.time.events.add(1000, () => {game.score.updatescore(value);
+this.scoretext.x=this.posx;
+this.scoretext.y=this.posy;
+this.scoretext.body.velocity.y=0;
+this.scoretext.text='';
+},this);
+
+
 }
 
 Arrive.prototype.interact = function(){
