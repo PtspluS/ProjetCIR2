b85e6092d586f7f70b8e40218e1b7f954893f8e9
Unknown <pierre.marez@isen.yncrea.fr>
2018-04-27 19:21:00 +0200

    la flemme de mettre un truc a chaque fois

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-var levels = [/*
+var levels = [
   {
     matrice: [
       [ 3,15, 3, 3, 3, 4, 3, 3, 4, 4, 3, 3, 3,22,22, 3, 3,22,22, 3, 3],
@@ -23,11 +23,11 @@ var levels = [/*
     polution : 10,
     spawnpoints: [[2,4],[3,4],[4,4],[5,4]],
     seauSpawnpoints: [],
-    imagePath: "assets/miniMap/level1.PNG",
+    imagePath: "assets/miniMap/Troll.PNG",
     name: "Level test",
     tutoText: [],
     tips : "Vous savez, moi je ne crois pas qu\’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd\’hui avec vous, je dirais que c\’est d\’abord des rencontres. Des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée... Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face je dirais, le miroir qui vous aide à avancer.",
-  },*/
+  },
   {
     matrice: [
       [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
