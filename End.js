357c68a72bb7eb0b618e188a5e2d6e639edfd52f
Pierricles <pierromanuel5@gmail.com>
2018-05-03 19:31:00 +0200

    bouton go fonctionel

--- End.js
+++ End.js
@@ -104,7 +104,20 @@ var end = {
     tip.fill = 'white';
     let go = end.add.button(end.world.width-350, posY+40*indication.length+60+140,'go',()=>{
       //if(this.equationResult>=levels[MenuGame.cursorMap].score){MenuGame.cursorMap += 1};
-      this.state.start('MenuGame');
+      MenuGame.cursorMap++;
+      if(levels[MenuGame.cursorMap].tutoText.length < 1){
+        game.id =   MenuGame.cursorMap;
+        game.playersskins = MenuGame.playersskins;
+        game.nbPlayers = MenuOpt.nbPlayers;
+        this.state.start('Game');
+      }else{
+        Tuto.id =   MenuGame.cursorMap;
+        game.id =  MenuGame.cursorMap;
+        game.playersskins = MenuGame.playersskins;
+        game.nbPlayers = MenuOpt.nbPlayers;
+        this.state.start('Tuto');
+      }
+
       this.musicEnd.stop();
 
     },this,1,0,2);
